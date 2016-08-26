5eb0a8b9446291f0a21fe9e997bdd4817a94f551
Shinya KOUKETSU <shinya.kouketsu@gmail.com>
2016-08-26 09:34:43 +0900

    Fix ndepth_ref check in gsw_saar.c
    As "NaN" for ndepth_ref is set to be 9e90 in gsw_saar_data.c,
     calculation on the "land" cause segmentation faults.
    To fix this, undef value for ndepth_ref is set to be "-1" and
     gsw_saar return GSW_INVALID_VALUE on the "land".

--- gsw_saar.c
+++ gsw_saar.c
@@ -29,7 +29,7 @@ function gsw_saar(p,long,lat)
 ! Calculates the Absolute Salinity Anomaly Ratio, SAAR.
 !
 ! p      : sea pressure                                    [dbar]
-! long   : longitude                                       [deg E]     
+! long   : longitude                                       [deg E]
 ! lat    : latitude                                        [deg N]
 !
 ! gsw_saar : Absolute Salinity Anomaly Ratio               [unitless]
@@ -74,12 +74,12 @@ gsw_saar(double p, double lon, double lat)
 	}
 
 	if (ndepth_max == -1.0)
-	    return (0.0);
+	    return (return_value);
 
 	if (p > p_ref[(int)(ndepth_max)-1])
 	    p	= p_ref[(int)(ndepth_max)-1];
 	indz0	= gsw_util_indx(p_ref,nz,p);
-    
+
 	r1	= (lon-longs_ref[indx0])/(longs_ref[indx0+1]-longs_ref[indx0]);
 	s1	= (lat-lats_ref[indy0])/(lats_ref[indy0+1]-lats_ref[indy0]);
 	t1	= (p-p_ref[indz0])/(p_ref[indz0+1]-p_ref[indz0]);
@@ -134,7 +134,7 @@ function gsw_deltasa_atlas(p,lon,lat)
 ! Calculates the Absolute Salinity Anomaly atlas value, delta_SA_atlas.
 !
 ! p      : sea pressure                                    [dbar]
-! lon    : longiture                                       [deg E]     
+! lon    : longiture                                       [deg E]
 ! lat    : latitude                                        [deg N]
 !
 ! deltasa_atlas : Absolute Salinity Anomaly atlas value    [g/kg]
@@ -179,12 +179,12 @@ gsw_deltasa_atlas(double p, double lon, double lat)
 	}
 
 	if (ndepth_max == -1.0)
-	    return (0.0);
+	    return (return_value);
 
 	if (p > p_ref[(int)(ndepth_max)-1])
 	    p	= p_ref[(int)(ndepth_max)-1];
 	indz0	= gsw_util_indx(p_ref,nz,p);
-    
+
 	r1	= (lon-longs_ref[indx0])/
 			(longs_ref[indx0+1]-longs_ref[indx0]);
 	s1	= (lat-lats_ref[indy0])/
