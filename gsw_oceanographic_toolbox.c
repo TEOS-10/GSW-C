e78f8c70f7f247266ce4501d55a3f4e341fbf11b
Dan Kelley <kelley.dan@gmail.com>
2017-08-06 08:53:23 -0300

    gsw_util_sort_real uses gsw-style indenting

--- gsw_oceanographic_toolbox.c
+++ gsw_oceanographic_toolbox.c
@@ -10615,21 +10615,28 @@ pure function gsw_util_sort_real (rarray) result(iarray)
 */
 
 typedef struct {
-    double d;
-    int i;
+	double	d;
+	int	i;
 } DI;
 
-int compareDI(const void *a, const void *b)
+/*
+ * Rank two items, by value if possible,
+ * and by inverse index, if the values are
+ * equal.
+ * FIXME: decide if index method matches docs.
+ */
+int
+compareDI(const void *a, const void *b)
 {
-    DI *A = (DI*)a;
-    DI *B = (DI*)b;
-    if (A->d < B->d)
-        return (-1);
-    if (A->d > B->d)
-        return (1);
-    if (A->i < B->i)
-        return (1);
-    return (-1);
+	DI	*A = (DI*)a;
+	DI	*B = (DI*)b;
+	if (A->d < B->d)
+		return (-1);
+	if (A->d > B->d)
+		return (1);
+	if (A->i < B->i)
+		return (1);
+	return (-1);
 }
 
 /*
@@ -10641,15 +10648,15 @@ void
 gsw_util_sort_real(double *rarray, int nx, int *iarray)
 {
 	int	i;
-        DI* di = (DI*)malloc(nx*sizeof(DI));
-        for (i=0; i<nx; i++) {
-            di[i].d = rarray[i];
-            di[i].i = i;
-        }
-        qsort(di, nx, sizeof(DI), compareDI);
+	DI* di = (DI*)malloc(nx*sizeof(DI));
+	for (i=0; i<nx; i++) {
+		di[i].d = rarray[i];
+		di[i].i = i;
+	}
+	qsort(di, nx, sizeof(DI), compareDI);
 	for (i=0; i<nx; i++)
-	    iarray[i] = di[i].i;
-        free(di);
+		iarray[i] = di[i].i;
+	free(di);
 }
 /*
 !==========================================================================
