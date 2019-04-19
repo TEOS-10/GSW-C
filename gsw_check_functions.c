97bbe104b29ff61db6bdcc21ae85ca569d9a41cd
Eric Firing <efiring@hawaii.edu>
2019-04-19 10:35:48 -1000

    p_from_z and z_from_p: add two arguments
    The two arguments, geo_strf_dyn_height and sea_surface_geopotential,
    are not used internally and are optional in the Matlab and Fortran
    versions.  They will also be optional in the Python version.  C support
    for optional arguments is less straightforward than in the other
    languages, so we stick with the simple 4-argument signature instead.

--- gsw_check_functions.c
+++ gsw_check_functions.c
@@ -156,8 +156,8 @@ main(int argc, char **argv)
         test_func(ct_from_pt, (sa[i],pt[i]), value,ct_from_pt);
         test_func(pt0_from_t, (sa[i],t[i],p[i]), value,pt0_from_t);
         test_func(pt_from_t, (sa[i],t[i],p[i],pref[0]), value,pt_from_t);
-        test_func(z_from_p, (p[i],lat[i]), z,z_from_p);
-        test_func(p_from_z, (z[i],lat[i]), value,p_from_z);
+        test_func(z_from_p, (p[i],lat[i], 0.0, 0.0), z,z_from_p);
+        test_func(p_from_z, (z[i],lat[i], 0.0, 0.0), value,p_from_z);
         test_func(entropy_from_pt, (sa[i],pt[i]), entropy,entropy_from_pt);
         test_func(pt_from_entropy, (sa[i],entropy[i]), value,pt_from_entropy);
         test_func(ct_from_entropy, (sa[i],entropy[i]), value,ct_from_entropy);
