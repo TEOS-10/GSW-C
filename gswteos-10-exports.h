fa4db1e18e90eac0a74bca5c24298131b7f638f9
egecetin <egecetin@hotmail.com.tr>
2021-07-10 20:50:35 +0300

    dll fix

---
+++ gswteos-10-exports.h
@@ -0,0 +1,18 @@
+#ifndef GSWTEOS10_SHARED_EXPORTS_H
+#define GSWTEOS10_SHARED_EXPORTS_H
+
+#ifdef GSWTEOS10_STATIC
+#  define GSWTEOS10_SHARED_EXPORTS
+#else
+#  ifndef GSWTEOS10_SHARED_EXPORTS
+#    ifdef GSWTEOS_10_EXPORTS
+        /* We are building this library */
+#      define GSWTEOS10_SHARED_EXPORTS __declspec(dllexport)
+#    else
+        /* We are using this library */
+#      define GSWTEOS10_SHARED_EXPORTS __declspec(dllimport)
+#    endif
+#  endif
+#endif
+
+#endif /* GSWTEOS10_SHARED_EXPORTS_H */
